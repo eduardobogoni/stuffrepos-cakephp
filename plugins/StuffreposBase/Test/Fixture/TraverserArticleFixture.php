fc10bfaa378b28c16024088f20b543fce92f72e4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2012-07-26 16:52:49 -0300

    Behavior to auto traverse database schema.

---
+++ plugins/StuffreposBase/Test/Fixture/TraverserArticleFixture.php
@@ -0,0 +1,27 @@
+<?php
+
+class TraverserArticleFixture extends CakeTestFixture {
+
+    public $fields = array(
+        'id' => array('type' => 'integer', 'key' => 'primary'),
+        'title' => array('type' => 'string', 'length' => 255, 'null' => false),
+        'author_id' => array('type' => 'integer'),
+    );
+    public $records = array(
+        array('id' => 1, 'title' => 'First Article', 'author_id' => 1),
+        array('id' => 2, 'title' => 'Second Article', 'author_id' => 2),
+        array('id' => 3, 'title' => 'Third Article', 'author_id' => 2),
+    );
+
+}
+
+class TraverserArticle extends Model {
+
+    public $actsAs = array('StuffreposBase.Traverser');
+    public $belongsTo = array(
+        'Author' => array(
+            'className' => 'TraverserAuthor'
+        )
+    );
+
+}
---
+++ plugins/StuffreposBase/Test/Fixture/TraverserAuthorFixture.php
@@ -0,0 +1,16 @@
+<?php
+
+class TraverserAuthorFixture extends CakeTestFixture {
+
+    public $fields = array(
+        'id' => array('type' => 'integer', 'key' => 'primary'),
+        'name' => array('type' => 'string', 'length' => 255, 'null' => false),
+    );
+    public $records = array(
+        array('id' => 1, 'name' => 'First Author'),
+        array('id' => 2, 'name' => 'Second Author'),
+    );
+}
+
+class TraverserAuthor extends Model {    
+}
