b3198ac5adcbdd6bb4349ed0aa9bda9e5dab02a4
Eduardo Henrique Bogoni <eduardo.bogoni@ifrj.edu.br>
2013-01-21 22:09:26 -0200

    Importação dos plugins Widgets e ExtendedScaffold por padrão.

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -2,3 +2,5 @@
 
 require_once(dirname(__FILE__).'/../../stuffrepos-cakephp/cakephp/app/Config/bootstrap.php');
 StuffreposBootstrap::run();
+CakePlugin::load('Widgets', array('bootstrap' => true));
+CakePlugin::load('ExtendedScaffold', array('bootstrap' => true));
