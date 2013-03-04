b29517b5e50067dbcfa071045ddc860c62515515
Eduardo Henrique Bogoni <eduardo.bogoni@ifrj.edu.br>
2013-03-04 10:31:25 -0300

    Configuração de diretório de temporários por usuário. Refs #120.

--- include.php
+++ include.php
@@ -12,3 +12,42 @@ ini_set(
 if (!defined('CORE_PATH')) {
     define('CORE_PATH', CAKE_CORE_INCLUDE_PATH . DS);
 }
+
+// Configuração de diretório de temporários por usuário.
+$userConfig = posix_getpwuid(posix_getuid());
+$cakephpTmpRoot = (empty($userConfig['dir']) ? sys_get_temp_dir() : $userConfig['dir'])
+        . DS . '.cakephp-tmp' . DS;
+$appName = defined('APP_ID') ? APP_ID : 'undefined-app-id';
+define('TMP', $cakephpTmpRoot . $appName . DS);
+
+function removeStringPrefix($prefix, $string) {
+    if (substr($string, 0, strlen($prefix)) == $prefix) {
+        return substr($string, strlen($prefix), strlen($string));
+    } else {
+        return $string;
+    }
+}
+
+function checkTemporaryDirectory($tmpSkeletonRootPath, DirectoryIterator $tmpSkeletonSubDir) {
+    while ($tmpSkeletonSubDir->valid()) {
+        if ($tmpSkeletonSubDir->isDir() && !$tmpSkeletonSubDir->isDot()) {
+            $relativePath = removeStringPrefix($tmpSkeletonRootPath, $tmpSkeletonSubDir->getRealPath());
+            $fullPath = TMP . $relativePath;
+
+            if (!file_exists($fullPath)) {
+                mkdir($fullPath, 0755, true);
+            }
+
+            checkTemporaryDirectory($tmpSkeletonRootPath, new DirectoryIterator($tmpSkeletonSubDir->getRealPath()));
+        }
+        $tmpSkeletonSubDir->next();
+    }
+}
+
+$tmpSkeletonRoot = new DirectoryIterator(dirname(__FILE__) . DS . 'cakephp' . DS . 'app' . DS . 'tmp');
+checkTemporaryDirectory($tmpSkeletonRoot->getRealPath(), $tmpSkeletonRoot);
+
+unset($userConfig);
+unset($cakephpTmpRoot);
+unset($appName);
+unset($tmpSkeletonRoot);
