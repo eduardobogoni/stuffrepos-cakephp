5d7bca346999d0c0e7fb2d6ea4613b9fa495b210
Eduardo Henrique Bogoni <eduardo.bogoni@ifrj.edu.br>
2013-03-08 20:27:50 -0300

    Refatoração do datasource Ldap / método read(). Refs #120.

--- plugins/Ldap/Test/Case/Model/Datasource/LdapTest.php
+++ plugins/Ldap/Test/Case/Model/Datasource/LdapTest.php
@@ -84,4 +84,84 @@ class LdapTest extends CakeTestCase {
             , true);
     }
 
+    public function testFindFirst() {
+        $this->assertEqual($this->LdapUser->find('first', array(
+                'conditions' => array(
+                    'LdapUser.username' => $this->sampleData['username']
+                )
+            )), array());
+
+        $this->LdapUser->create();
+
+        $this->LdapUser->save(
+            array(
+                'LdapUser' => $this->sampleData
+            )
+        );
+
+        $result = $this->LdapUser->find('first', array(
+            'conditions' => array(
+                'LdapUser.username' => $this->sampleData['username']
+            )
+            ));
+
+        $this->assertNotEqual($result, false);
+        $this->assertNotEqual($result, array());
+        $this->assertEqual(empty($result[$this->LdapUser->alias]['username']), false);        
+        $this->assertEqual($result[$this->LdapUser->alias]['username'], $this->sampleData['username']);
+    }
+
+    public function testFindAll() {
+
+        $this->assertEqual($this->LdapUser->find('all', array(
+                'conditions' => array(
+                    'LdapUser.username' => $this->sampleData['username']
+                )
+            )), array());
+
+        $this->LdapUser->create();
+
+        $this->LdapUser->save(
+            array(
+                'LdapUser' => $this->sampleData
+            )
+        );
+
+        $result = $this->LdapUser->find('all', array(
+            'conditions' => array(
+                'LdapUser.username' => $this->sampleData['username']
+            )
+            ));
+
+        $this->assertNotEqual($result, false);
+        $this->assertNotEqual($result, array());
+        $this->assertEqual(count($result), 1);
+        $this->assertEqual(empty($result[0][$this->LdapUser->alias]), false);
+        $this->assertEqual($result[0][$this->LdapUser->alias]['username'], $this->sampleData['username']);
+    }
+
+    public function testFindCount() {
+        $this->assertEqual($this->LdapUser->find('count', array(
+                'conditions' => array(
+                    'LdapUser.username' => $this->sampleData['username']
+                )
+            )), 0);
+
+        $this->LdapUser->create();
+
+        $this->LdapUser->save(
+            array(
+                'LdapUser' => $this->sampleData
+            )
+        );
+
+        $result = $this->LdapUser->find('count', array(
+            'conditions' => array(
+                'LdapUser.username' => $this->sampleData['username']
+            )
+            ));
+
+        $this->assertEqual($result, 1);
+    }
+
 }
