4c61cd894d6c445d456347b1df78831e609953ca
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2013-03-18 21:15:22 -0300

    Corrige shell de migração de todos os plugins de acordo com a versão 2.2 do Migrations.

--- plugins/Base/Console/Command/MigrationAllPluginsShell.php
+++ plugins/Base/Console/Command/MigrationAllPluginsShell.php
@@ -5,20 +5,16 @@ App::import('Lib', 'Migrations.MigrationVersion');
 class MigrationAllPluginsShell extends Shell {
 
     public function main() {
-        $version = & new MigrationVersion(
-                        array(
-                            'connection' => 'default',
-                            'autoinit' => true
-                        )
-        );
+        $version = new MigrationVersion();
 
         foreach (CakePlugin::loaded() as $plugin) {
             if ($version->getMapping($plugin)) {
-                $this->dispatchShell('Migrations.migration', '--plugin', $plugin, 'all');
+                $this->out('Migrating plugin '.$plugin);
+                $this->dispatchShell('Migrations.migration', 'run', '--plugin', $plugin, 'all');
             }
         }
 
-        $this->dispatchShell('Migrations.migration', 'all');
+        $this->dispatchShell('Migrations.migration', 'run', 'all');
     }
 
 }
