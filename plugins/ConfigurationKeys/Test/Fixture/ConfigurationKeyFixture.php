dbb8c5ca0fe0ddfab9b360f6433fdaf89a1ec4f1
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2013-06-28 12:22:31 -0300

    Fixtures com importação de esquema de banco de dados em vez de definição estática.

--- plugins/ConfigurationKeys/Test/Fixture/ConfigurationKeyFixture.php
+++ plugins/ConfigurationKeys/Test/Fixture/ConfigurationKeyFixture.php
@@ -2,16 +2,6 @@
 
 class ConfigurationKeyFixture extends CakeTestFixture {
 
-    public $fields = array(
-        'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
-        'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-        'description' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-        'default_value' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-        'setted_value' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-        'current_value' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-        'setted' => array('type' => 'boolean'),
-    );
-    public $records = array(
-    );
+    public $import = array('model' => 'ConfigurationKeys.ConfigurationKey');
 
 }
--- plugins/ConfigurationKeys/Test/Fixture/SettedConfigurationKeyFixture.php
+++ plugins/ConfigurationKeys/Test/Fixture/SettedConfigurationKeyFixture.php
@@ -2,18 +2,6 @@
 
 class SettedConfigurationKeyFixture extends CakeTestFixture {
 
-    public $fields = array(
-        'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
-        'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-        'value' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-    );    
+    public $import = array('model' => 'ConfigurationKeys.SettedConfigurationKey');
     
-    public $records = array(
-    );
-        
 }
-
-/*
-class SettedConfigurationKey extends Model{
-    
-}*/
