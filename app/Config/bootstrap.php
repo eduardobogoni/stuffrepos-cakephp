08d14e8a6979d273850bee9d189a1863164d2694
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2013-07-15 20:38:02 -0300

    Adiciona corretamente o bootstrap do stuffrepos-cakephp.

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -1,6 +1,10 @@
 <?php
 
-require_once(dirname(__FILE__).'/../../stuffrepos-cakephp/cakephp/app/Config/bootstrap.php');
-StuffreposBootstrap::run();
+if (!defined('DS')) {
+    define('DS', DIRECTORY_SEPARATOR);
+}
+
+require_once(dirname(dirname(dirname(__FILE__))) . DS . 'stuffrepos-cakephp' . DS . 'cakephp' . DS . 'app' . DS . 'Config' . DS . 'bootstrap.php');
+require_once(dirname(dirname(dirname(__FILE__))) . DS . 'stuffrepos-cakephp' . DS . 'bootstrap.php');
 CakePlugin::load('Widgets', array('bootstrap' => true));
 CakePlugin::load('ExtendedScaffold', array('bootstrap' => true));
