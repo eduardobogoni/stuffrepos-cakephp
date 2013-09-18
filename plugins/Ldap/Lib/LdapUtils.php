bbb0dabe57ba00cebbab1be5902a7cd97809aa60
Eduardo Henrique Bogoni <eduardo.bogoni@ifrj.edu.br>
2013-09-18 15:04:31 -0300

    Método para juntar DNs Ldap. Refs #8065.

--- plugins/Ldap/Lib/LdapUtils.php
+++ plugins/Ldap/Lib/LdapUtils.php
@@ -68,4 +68,15 @@ class LdapUtils {
                 return $explodedDn[0];
         }
     }
+
+    public static function joinDns($dn1, $dn2) {
+        $dn1 = trim($dn1);
+        $dn2 = trim($dn2);
+        if ($dn1 != '' && $dn2 != '') {
+            return $dn1 . ',' . $dn2;
+        } else {
+            return $dn1 . $dn2;
+        }
+    }
+
 }
