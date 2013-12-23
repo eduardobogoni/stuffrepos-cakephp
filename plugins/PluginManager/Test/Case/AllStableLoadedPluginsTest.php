e4ac887dd006f63236ec33a5f578fc25afdc7091
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2013-12-23 20:08:13 -0200

    Suíte de testes estáveis ignora plugin "Tools".

--- plugins/PluginManager/Test/Case/AllStableLoadedPluginsTest.php
+++ plugins/PluginManager/Test/Case/AllStableLoadedPluginsTest.php
@@ -1,11 +1,16 @@
 <?php
 
 class AllLoadedPluginsNoMigrationsTest extends PHPUnit_Framework_TestSuite {
+    
+    private static $noStable = array(
+        'Migrations'
+        , 'Tools'
+    );
 
     public static function suite() {
         $suite = new CakeTestSuite('All Plugins');
         foreach (CakePlugin::loaded() as $plugin) {
-            if ($plugin != 'Migrations') {
+            if (!in_array($plugin, self::$noStable)) {
                 $suite->addTestDirectoryRecursive(CakePlugin::path($plugin) . DS . 'Test' . DS . 'Case');
             }
         }
