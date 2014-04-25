3a3401b9ada25b81e45eb207283b0ef60fe2dcf9
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2014-04-25 18:38:21 -0300

    Gera chaves de segurança no esqueleto da aplicação.

--- app/Config/core.php
+++ app/Config/core.php
@@ -1,13 +1,3 @@
 <?php
 
-if (!defined('DS')) {
-    define('DS', DIRECTORY_SEPARATOR);
-}
-
-require_once(dirname(dirname(dirname(__FILE__))) . DS . 'stuffrepos-cakephp' . DS . 'cakephp' . DS . 'app' . DS . 'Config' . DS . 'bootstrap.php');
-require_once(dirname(dirname(dirname(__FILE__))) . DS . 'stuffrepos-cakephp' . DS . 'bootstrap.php');
-PluginManager::init('app', array(
-    'AccessControl',
-    'ExtendedScaffold',
-    'Widgets',
-));
+require_once(dirname(dirname(dirname(__FILE__))) . DS . 'stuffrepos-cakephp' . DS . 'cakephp' . DS . 'app' . DS . 'Config' . DS . 'core.php');
--- scripts/create-app-directory.sh
+++ scripts/create-app-directory.sh
@@ -23,3 +23,25 @@ mkdir -p "$APP_PATH"
 (cd "$STUFFREPOS_DIRECTORY/cakephp/app"; cp . "$APP_PATH/" -R)
 (cd "$STUFFREPOS_DIRECTORY/app"; cp . "$APP_PATH/" -R)
 echo `find "$APP_PATH/tmp" -type d -exec chmod og+wt '{}' \;`
+php "$STUFFREPOS_DIRECTORY/scripts/gen-security.php" >> "$APP_PATH/Config/core.php"
+cp "$APP_PATH/Config/database.php.default" "$APP_PATH/Config/database.php"
+cp "$APP_PATH/Config/email.php.default" "$APP_PATH/Config/email.php"
+
+rm -rf "$APP_PATH/Config/Schema"
+rm -rf "$APP_PATH/Config/acl.ini.php"
+rm -rf "$APP_PATH/Config/acl.php"
+rm -rf "$APP_PATH/webroot/css/cake.generic.css"
+rm -rf "$APP_PATH/View/Errors"
+rm -rf "$APP_PATH/View/Emails"
+rm -rf "$APP_PATH/View/Layouts/Emails"
+rm -rf "$APP_PATH/View/Layouts/js"
+rm -rf "$APP_PATH/View/Layouts/rss"
+rm -rf "$APP_PATH/View/Layouts/xml"
+rm -rf "$APP_PATH/View/Layouts/ajax.ctp"
+rm -rf "$APP_PATH/View/Layouts/error.ctp"
+rm -rf "$APP_PATH/View/Layouts/flash.ctp"
+find "$APP_PATH" -name empty -type f -delete
+find "$APP_PATH" -type d -empty -delete
+tree "$APP_PATH"
+
+
