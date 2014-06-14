e3c4496625606fa9f93b9831b63c89edfe800244
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2014-06-13 23:53:23 -0300

    Parser de emails em arquivo.

---
+++ plugins/Email/Lib/MailParser.php
@@ -0,0 +1,86 @@
+<?php
+
+App::uses('PlancakeEmailParser', 'Email.Lib/PlancakeEmailParser');
+
+class MailParser {
+
+    public static function parseFile($path) {
+        return self::parse(file_get_contents($path));
+    }
+
+    public static function parse($content) {
+        $main = self::_parseRawMail($content);
+        $ret = array();
+        foreach (array('to', 'from', 'bcc', 'cc') as $header) {
+            $ret[$header] = empty($main['headers'][$header]) ?
+                    array() :
+                    self::_buildAddressList($main['headers'][$header]);
+        }
+        foreach (array('reply_to', 'sender') as $header) {
+            $ret[$header] = empty($main['headers'][$header]) ?
+                    self::_buildAddressList($main['headers']['from']) :
+                    self::_buildAddressList($main['headers'][$header]);
+        }
+        foreach (array('subject', 'in_reply_to', 'message-id') as $header) {
+            $ret[str_replace('-', '_', $header)] = empty($main['headers'][$header]) ? '' : $main['headers'][$header];
+        }
+
+        $ret['date'] = new Horde_Imap_Client_DateTime($main['headers']['date']);
+        $ret['bodies'] = self::_buildBodies($main);
+        return $ret;
+    }
+
+    private static function _mimeDecode($string) {
+        return iconv_mime_decode($string, 0, mb_internal_encoding());
+    }
+
+    private static function _parseRawMail($content) {
+        $mimemail = mailparse_msg_create();
+        mailparse_msg_parse($mimemail, $content);
+        $parsedData = mailparse_msg_get_part_data($mimemail);
+        $parsedData['body'] = self::_extractBody($parsedData, $content);
+        if (!empty($parsedData['headers']['subject'])) {
+            $parsedData['headers']['subject'] = self::_mimeDecode($parsedData['headers']['subject']);
+        }
+        mailparse_msg_free($mimemail);
+        return $parsedData;
+    }
+
+    private static function _buildBodies($parsedData) {
+        if ($parsedData['content-type'] == 'multipart/alternative') {
+            return self::_buildBodiesFromMultipart($parsedData['body'], $parsedData['content-boundary']);
+        } else {
+            return array(
+                $parsedData['content-type'] => self::_mimeDecode($parsedData['body'])
+            );
+        }
+    }
+
+    private static function _buildBodiesFromMultipart($body, $boundary) {
+        $parts = explode("--" . $boundary, $body);
+        array_shift($parts);
+        array_pop($parts);
+        $bodies = array();
+        foreach ($parts as $part) {
+            $partRawData = preg_replace("/\r\n\$/", '', ltrim($part));
+            $parsedData = self::_parseRawMail($partRawData);
+            $bodies[$parsedData['content-type']] = $parsedData['body'];
+        }
+        return $bodies;
+    }
+
+    private static function _extractBody($parsedData, $content) {
+        $body = substr($content, $parsedData['starting-pos-body'], $parsedData['ending-pos-body'] - $parsedData['starting-pos-body']);
+        if (!empty($parsedData['transfer-encoding']) && $parsedData['transfer-encoding'] == 'quoted-printable') {
+            $body = quoted_printable_decode($body);
+        }
+        return $body;
+    }
+
+    private static function _buildAddressList($string) {
+        return array_map(function($v) {
+            return $v['address'];
+        }, mailparse_rfc822_parse_addresses($string));
+    }
+
+}
---
+++ plugins/Email/Test/Case/Lib/MailParserTest.php
@@ -0,0 +1,75 @@
+<?php
+
+App::uses('MailParser', 'Email.Lib');
+
+class MailParserTest extends CakeTestCase {
+
+    public function testParseMail1() {
+        $mailData = MailParser::parseFile(dirname(__FILE__) . DS . 'mail1');
+        $mailDataDate = $mailData['date'];
+        unset($mailData['date']);
+        $expectedDate = new DateTime('2014-06-13 18:18:41 -03:00');
+        $this->assertEqual($mailDataDate instanceof DateTime, true);
+        $this->assertEqual($mailDataDate->getTimestamp(), $expectedDate->getTimestamp());
+        $this->assertEqual($mailDataDate->getOffset(), $expectedDate->getOffset());
+        $this->assertEqual($mailData, array(
+            'bcc' => array(),
+            'cc' => array(),
+            'from' => array(
+                'eduardobogoni@gmail.com'
+            ),
+            'in_reply_to' => '',
+            'message_id' => '<CAAGzLnaN1dn+dAsPeCrCyy5c_dU2ytfQ3qvNmjM7T00QdcLu8A@mail.gmail.com>',
+            'reply_to' => array(
+                'eduardobogoni@gmail.com'
+            ),
+            'sender' => array(
+                'eduardobogoni@gmail.com'
+            ),
+            'subject' => 'Hello',
+            'to' => array(
+                'ehbrsmailer@gmail.com'
+            ),
+            'bodies' => array(
+                'text/plain' => 'Hello World!' . "\r\n",
+                'text/html' => '<div dir="ltr">Hello World!<br></div>' . "\r\n",
+            )
+        ));
+    }
+
+    public function testParseMail2() {
+        $mailData = MailParser::parseFile(dirname(__FILE__) . DS . 'mail2');
+        $mailDataDate = $mailData['date'];
+        unset($mailData['date']);
+        $expectedDate = new DateTime('2014-06-13 22:22:40 -03:00');
+        $this->assertEqual($mailDataDate instanceof DateTime, true);
+        $this->assertEqual($mailDataDate->getTimestamp(), $expectedDate->getTimestamp());
+        $this->assertEqual($mailDataDate->getOffset(), $expectedDate->getOffset());
+        $this->assertEqual($mailData, array(
+            'bcc' => array(),
+            'cc' => array(),
+            'from' => array(
+                'eduardobogoni@gmail.com'
+            ),
+            'in_reply_to' => '',
+            'message_id' => '<CAAGzLnbD57Z6BX6SxMbdffgOZ8X3p=oSMx1-RWp=64jsyym6JQ@mail.gmail.com>',
+            'reply_to' => array(
+                'eduardobogoni@gmail.com'
+            ),
+            'sender' => array(
+                'eduardobogoni@gmail.com'
+            ),
+            'subject' => 'Palavras com acentos (Acento no assunto: é)',
+            'to' => array(
+                'eduardobogoni@gmail.com',
+                'ehbrsmailer@gmail.com'
+            ),
+            'bodies' => array(
+                'text/plain' => 'Acento no corpo da mensagem: á' . "\r\n",
+                'text/html' => '<div dir="ltr">Acento no corpo da mensagem: á<br></div>' . "\r\n"
+            )
+                )
+        );
+    }
+
+}
