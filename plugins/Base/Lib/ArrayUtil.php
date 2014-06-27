726b3707eab3c722beb0576dcdcf6ff4474d0bd5
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2014-06-27 15:17:38 -0300

    Ordena arrays por nomes de campos.

--- plugins/Base/Lib/ArrayUtil.php
+++ plugins/Base/Lib/ArrayUtil.php
@@ -100,6 +100,22 @@ class ArrayUtil {
         $ref = $value;
     }
 
+    public static function orderBy() {
+        $args = func_get_args();
+        $data = array_shift($args);
+        foreach ($args as $n => $field) {
+            if (is_string($field)) {
+                $tmp = array();
+                foreach ($data as $key => $row)
+                    $tmp[$key] = $row[$field];
+                $args[$n] = $tmp;
+            }
+        }
+        $args[] = &$data;
+        call_user_func_array('array_multisort', $args);
+        return array_pop($args);
+    }
+
 }
 
 ?>
