5c478f796391d968ebc6394c50bf9cbf4e31205f
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2014-06-28 19:44:33 -0300

    Convert arquivo CSV com nomes de colunas.

--- plugins/Base/Lib/CsvUtil.php
+++ plugins/Base/Lib/CsvUtil.php
@@ -13,4 +13,33 @@ class CsvUtil {
         return $lines;
     }
 
+    public static function fileToArrayWithColumns($filePath,
+            $convertFunction = null) {
+        $lines = self::fileToArray($filePath);
+        $columns = array_shift($lines);
+        if (!$columns) {
+            throw new Exception("File \"$filePath\" has no columns line");
+        }
+        $ret = array();
+        foreach ($lines as $row) {
+            $ret[] = self::_rawRowToAssociativeRow($row, $columns, $convertFunction);
+        }
+        return $ret;
+    }
+
+    private static function _rawRowToAssociativeRow($row, $columns,
+            $convertFunction) {
+        $ret = array();
+        foreach ($columns as $columnIndex => $columnName) {
+            $ret[$columnName] = self::_rowValue($row[$columnIndex], $columnName, $convertFunction);
+        }
+        return $ret;
+    }
+
+    private static function _rowValue($value, $column, $convertFunction) {
+        return $convertFunction ?
+                call_user_func($convertFunction, $column, $value) :
+                $value;
+    }
+
 }
