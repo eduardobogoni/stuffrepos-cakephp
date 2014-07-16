7c792024f9a86caa5c3beb9cf3080f8b1770d277
Eduardo Henrique Bogoni <eduardo.bogoni@ifrj.edu.br>
2014-07-16 11:13:14 -0300

    Recupera listas para views a partir de validações inList.

--- plugins/Base/Model/Behavior/ExtendedOperationsBehavior.php
+++ plugins/Base/Model/Behavior/ExtendedOperationsBehavior.php
@@ -93,6 +93,56 @@ class ExtendedOperationsBehavior extends ModelBehavior {
         }
     }
     
+    public function  valuesFromValidationInList(\Model $model, $field) {
+        if (($validate = $this->_fieldInListValidation($model, $field))) {
+            if (!empty($validate['rule'][1]) && is_array($validate['rule'][1])) {
+                return $validate['rule'][1];
+            }
+            else {
+                throw new Exception("\"{$model->name}.$field\" inList validation has no list: ".print_r($validate, true));
+            }
+            
+        } else {
+            throw new Exception("List validation not found for field \"{$model->name}.$field\"");
+        }
+    }
+
+    public function listFromValidationInList(\Model $model, $field) {
+        $values = $this->valuesFromValidationInList($model, $field);
+        sort($values);
+        return array_merge(
+                array(
+            '' => '-- SELECIONE --',
+                ), ArrayUtil::keysAsValues($values)
+        );
+    }
+
+    public function listsFromValidationsInList(\Model $model) {
+        $ret = array();
+        foreach(array_keys($model->schema()) as $field) {
+            if ( $this->_fieldInListValidation($model,$field) ) {
+                $ret[Inflector::variable(Inflector::pluralize($field))] = $this->listFromValidationInList($model, $field);
+            }
+        }
+        return $ret;
+    }
+
+    /**
+     * Retorna a validação inList de um campo se existir.
+     * @param mixed $fieldName O array da validação ou FALSE caso não exista
+     * uma validação inList para o campo.
+     */
+    private function _fieldInListValidation(\Model $model, $field) {
+        if (!empty($model->validate[$field])) {
+            foreach ($model->validate[$field] as $validate) {
+                if (!empty($validate['rule'][0]) && $validate['rule'][0] == 'inList' && !empty($validate['rule'][1])) {
+                    return $validate;
+                }
+            }
+        }
+        return false;
+    }
+
     private function _parseVirtualFields(\Model $model) {
         $ret = array();
         foreach($model->getVirtualFields() as $name => $data) {
