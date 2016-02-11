d6915c78807e1387fc48d5683681a9a2dd9d27b0
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2016-02-11 00:37:52 -0200

    OfxUtil autodetecta a codificação de texto.

--- plugins/Base/Lib/OfxUtil.php
+++ plugins/Base/Lib/OfxUtil.php
@@ -13,25 +13,30 @@ class OfxUtil {
      * @throws Exception
      */
     public static function fileToXml($path) {
-        $handle = fopen($path, 'r');
-        if ($handle === false) {
-            throw new Exception("Could not read file \"$path\"");
-        }
+        $skip = true;
         $data = '';
-        while (!feof($handle)) {
-            $line = trim(fgets($handle));
-            if ($line == '' || preg_match('/^.+\:.+$/', $line)) {
-                continue;
-            } else {
-                $data = $line . PHP_EOL;
-                break;
+        foreach(self::readLines($path) as $line) {
+            $l = trim($line);
+            if ($skip && !($l == '' || preg_match('/^.+\:.+$/', $l))) {
+                $data = $l . PHP_EOL;
+                $skip = false;
+            }
+            else {
+                $data .= $line . PHP_EOL;
             }
         }
-        while (!feof($handle)) {
-            $data .= fread($handle, 4096);
-        }
         $data = str_replace('&', '&amp;', $data);
+        file_put_contents('/tmp/teste.xml', $data);
         return new SimpleXMLElement($data);        
+    }
+
+    private static function readLines($path) {
+      if (!file_exists($path)) {
+          throw new Exception("Could not read file \"$path\"");
+      }
+      $data = file_get_contents($path);
+      $enc = mb_detect_encoding($data);
+      return explode("\n", mb_convert_encoding($data, $enc));
     }
 
 }
